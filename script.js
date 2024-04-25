1c2b1887fd785abb7244b7ba2e036e85f159343e
sivakumar9392 <168119364+sivakumar9392@users.noreply.github.com>
2024-04-25 22:41:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+    console.log("Name: " + name);
+    console.log("Email: " + email);
+    console.log("Message: " + message);
+});
